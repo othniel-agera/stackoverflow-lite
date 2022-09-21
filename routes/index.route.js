ca516598211830932faa6802bd18ebf51e771854
othniel-agera <othniel.agera@flutterwavego.com>
2022-09-21 13:23:08 +0100

    More Review Changes

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,13 +4,6 @@ const router = express.Router();
 const userRoute = require('./user.route');
 const questionRoute = require('./question.route');
 
-// eslint-disable-next-line no-unused-vars
-router.use((err, req, res, _next) => {
-  res.status(500).json({
-    message: 'An error occurred while processing your request',
-  });
-});
-
 router.use('/auth', userRoute);
 router.use('/questions', questionRoute);
 
@@ -23,6 +16,13 @@ router.get('/', (req, res) => {
 router.all('*', (req, res) => {
   res.status(404).json({
     message: 'Invalid request, Route does not exist',
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, _next) => {
+  res.status(500).json({
+    message: 'An error occurred while processing your request',
   });
 });
 
