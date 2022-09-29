e278a2f4a50704c297d4ac8e6d342e731df7263e
othniel-agera <othniel.agera@flutterwavego.com>
2022-09-29 08:11:07 +0100

    [finishes #183406614] Made corrections

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -51,7 +51,9 @@ describe('User Registration Test', () => {
       expect(resp_data).to.have.property('accessToken');
       expect(resp_data).to.have.property('refreshToken');
       expect(resp_data.accessToken).to.be.an('string');
+      expect(resp_data.accessToken).to.not.equal('');
       expect(resp_data.refreshToken).to.be.an('string');
+      expect(resp_data.refreshToken).to.not.equal('');
     });
   });
 
