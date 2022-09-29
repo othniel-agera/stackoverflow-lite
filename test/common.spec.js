71895ccdf88f1250bb930adad042ef6c72f10347
othniel-agera <othniel.agera@flutterwavego.com>
2022-09-29 09:44:18 +0100

    [finishes #183413762] Test questions

--- test/common.spec.js
+++ test/common.spec.js
@@ -3,9 +3,29 @@ const url = require('url');
 const { expect } = require('chai');
 const { app } = require('../server');
 
+const getRequest = (route, token) => request(app)
+  .get(route)
+  .set('Authorization', token)
+  .set('Accept', 'application/json');
+const postRequest = (route, token) => request(app)
+  .post(route)
+  .set('Authorization', token)
+  .set('Accept', 'application/json');
+const putRequest = (route, token) => request(app)
+  .put(route)
+  .set('Authorization', token)
+  .set('Accept', 'application/json');
+const deleteRequest = (route, token) => request(app)
+  .delete(route)
+  .set('Authorization', token)
+  .set('Accept', 'application/json');
 module.exports = {
   expect,
   app,
   url,
   request,
+  getRequest,
+  postRequest,
+  putRequest,
+  deleteRequest,
 };
